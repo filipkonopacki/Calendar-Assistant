81efd8289287dc1ddcb403200b1c1b218426af8d
Filip <filip.konopacki96@gmail.com>
2025-08-01 18:34:02 +0200

    Fix paths definitions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,23 +1,18 @@
 import os
-import dotenv
-from datetime import datetime
-from jinja2 import Environment, FileSystemLoader
+from dotenv import load_dotenv
 
+load_dotenv()
 
-dotenv.load_dotenv()
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-PROMPT_DIR = os.path.join(ROOT_DIR, '..', 'prompt_templates')
-
-env = Environment(loader=FileSystemLoader(PROMPT_DIR))
-SYS_MSG = env.get_template('sys_msg.jinja2').render(today=datetime.now())
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+PROMPT_DIR = os.path.join(ROOT_DIR, 'prompt_templates')
 
 # Define Google services credentials and token file paths
-GOOGLE_CREDENTIALS = os.path.join(ROOT_DIR, '..', 'credentials.json')
-GOOGLE_TOKEN = os.path.join(ROOT_DIR, '..', 'token.json')
+GOOGLE_CREDENTIALS = os.path.join(ROOT_DIR, 'credentials.json')
+GOOGLE_TOKEN = os.path.join(ROOT_DIR, 'token.json')
 
 # Define the path for the logs directory
-LOGS_DIR = os.path.join(ROOT_DIR, '..', 'logs')
+LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
 if not os.path.exists(LOGS_DIR):
     os.makedirs(LOGS_DIR)
